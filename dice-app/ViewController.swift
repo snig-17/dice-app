2c0a1c550977b4b222d23d086d2caadb72ba1c48
snig-17 <snigdha.17.tiwari@icloud.com>
2025-08-31 22:01:17 +0530

    Initial Commit

---
+++ dice-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice-app
+//
+//  Created by Snigdha Tiwari  on 31/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dice-appTests/dice_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  dice_appTests.swift
+//  dice-appTests
+//
+//  Created by Snigdha Tiwari  on 31/08/2025.
+//
+
+import Testing
+@testable import dice_app
+
+struct dice_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
