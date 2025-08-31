ed328f8b94a353c9583fa93a140fb0801b096620
snig-17 <snigdha.17.tiwari@icloud.com>
2025-08-31 22:09:36 +0530

    UI complete and variables created

--- dice-app/ViewController.swift
+++ dice-app/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice1ImageView: UIImageView!
+    @IBOutlet weak var dice2ImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollDicePressed(_ sender: Any) {
+    }
+    
 }
 
